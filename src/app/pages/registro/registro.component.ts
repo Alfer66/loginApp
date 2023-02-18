2c9bd0eef15138d362e429404a2973b908f53c63
LOCAL\alferr <alferr@lobos.com.mx>
2023-02-17 19:04:08 -0600

    2023_02_17_1903_ARV
    Avance del curso de Angular

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  usuario: UsuarioModel;
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    this.usuario = new UsuarioModel();
+
+    this.usuario.email = 'fernando.herrera85@gmail.com';
+  }
 
 
 }
